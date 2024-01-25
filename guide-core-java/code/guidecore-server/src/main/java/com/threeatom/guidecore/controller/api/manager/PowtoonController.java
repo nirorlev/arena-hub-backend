4490f9b7c575a787e3dea0d12116f83f651efccf
huangpei2814 <huangpei0318>
2024-01-25 18:38:48 +0800

    pt:配置查询修改

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/controller/api/manager/PowtoonController.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/controller/api/manager/PowtoonController.java
@@ -1836,8 +1836,11 @@ public class PowtoonController extends GuideCoreController {
 		queryWrapper.eq("master_id", master.getId());
 		List<PtConfig> config = ptConfigService.list(queryWrapper);
 		PtConfig ptConfig = new PtConfig();
-		if (config.size()!=TableConstant.COMMON_ONE){
-			ptConfig = null;
+		if (null==config){
+			return new Message().ok().addData("master",master).addData("config",null);
+		}
+		if (null!=config&&config.size()<=TableConstant.COMMON_ONE){
+			ptConfig = config.get(TableConstant.COMMON_ZERO);
 		}else {
 			ptConfig = config.get(config.size()-TableConstant.COMMON_ONE);
 		}
