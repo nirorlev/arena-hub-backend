84767e9fb580fe1998b3f1a08cf1df1a4b9a66f8
huangpei2814 <huangpei0318>
2024-01-31 10:42:02 +0800

    pt:头像返回

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/GcUserSaveFolderServiceImpl.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/GcUserSaveFolderServiceImpl.java
@@ -168,6 +168,7 @@ public class GcUserSaveFolderServiceImpl extends ServiceImpl<GcUserSaveFolderMap
             }
             if (null!=gcUserSaveFolder.getUser().getInfo().getAvatarFileId()){
                 SysFile sysFile =sysFileService.getById(gcUserSaveFolder.getUser().getInfo().getAvatarFileId());
+                sysFile.setFullFileUrl(sysFileService.getResFullUrl(sysFile,request));
                 gcUserSaveFolder.getUser().getInfo().setAvatarFile(sysFile);
             }
 
