7698fedd117b06d78f76d0a935dd4ad5c9ced2f5
huangpei2814 <huangpei0318>
2024-02-02 19:08:30 +0800

    pt:数量修复

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/GvgMasterServiceImpl.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/GvgMasterServiceImpl.java
@@ -1853,6 +1853,7 @@ public class GvgMasterServiceImpl extends ServiceImpl<GcMasterMapper, GcMaster> 
 			thisUserEventAnswer.setUser(gcUser);
 		}
 
+
 		Message m = new Message().ok();
 		m.addData("参数说明", "otherUserId-点他人头像时，传该用户的id，可空，空时返回本人的数据");
 		m.addData("thisUserEventAnswer", thisUserEventAnswer);
