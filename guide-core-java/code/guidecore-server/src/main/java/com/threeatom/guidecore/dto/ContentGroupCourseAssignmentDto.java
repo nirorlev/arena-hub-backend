7cacd6d5b6b69753b20ca044c03e7e6719e7971c
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-02-27 07:05:02 +0200

    VCP-14776: add swagger annotations

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/ContentGroupCourseAssignmentDto.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/ContentGroupCourseAssignmentDto.java
@@ -1,21 +1,34 @@
 package com.threeatom.guidecore.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel(description = "Data Transfer Object representing a content group course assignment")
 public class ContentGroupCourseAssignmentDto {
 
+    @ApiModelProperty(notes = "The unique ID of the content group course assignment")
     private Integer id;
 
+    @ApiModelProperty(notes = "The title of the course")
     private String courseTitle;
+
+    @ApiModelProperty(notes = "The ID of the course")
     private String courseId;
+
+    @ApiModelProperty(notes = "The URL of the course image")
     private String courseImageUrl;
 
+    @ApiModelProperty(notes = "Whether the course is mandatory or not")
     private Boolean mandatory;
 
+    @ApiModelProperty(notes = "The source of the content group course assignment")
     private ContentGroupCourseAssignmentSourceDto source;
+
+    @ApiModelProperty(notes = "The date and time when the content group course assignment was last modified")
     private LocalDateTime modifiedDate = LocalDateTime.now();
 }
