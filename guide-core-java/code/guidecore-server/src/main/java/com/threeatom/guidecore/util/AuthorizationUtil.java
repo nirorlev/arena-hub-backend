7bef7311dd8a7ee545724e05b8c9521e34d76e82
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-03-19 14:06:42 +0200

    VCP-15207: add masterId verification filter

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/util/AuthorizationUtil.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/util/AuthorizationUtil.java
@@ -2,8 +2,10 @@ package com.threeatom.guidecore.util;
 
 import com.threeatom.common.jwt.JwtUtil;
 import com.threeatom.guidecore.enums.UserRole;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import lombok.experimental.UtilityClass;
+import org.springframework.util.StringUtils;
 
 @UtilityClass
 public class AuthorizationUtil {
@@ -13,13 +15,29 @@ public class AuthorizationUtil {
     private static final String AUTHORIZATION = "Authorization";
 
     public String getRole(HttpServletRequest request) {
-        String token = request.getHeader(AUTHORIZATION);
+        String token = getRequestAuthHeader(request);
+        if (token == null) {
+            return null;
+        }
+
         return JwtUtil.getValueByToken(token, ROLE);
     }
 
+    public static Optional<String> getPayloadValueByName(String token, String name) {
+        if (StringUtils.isEmpty(token) || StringUtils.isEmpty(name)) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(JwtUtil.getValueByToken(token, name));
+    }
+
     public Integer getUserUid(HttpServletRequest request) {
-        String token = request.getHeader(AUTHORIZATION);
+        String token = getRequestAuthHeader(request);
         return Integer.valueOf(JwtUtil.getValueByToken(token, UID));
+    }
+
+    public static String getRequestAuthHeader(HttpServletRequest request) {
+        return request.getHeader(AUTHORIZATION);
     }
 
     public boolean isManager(HttpServletRequest request) {
