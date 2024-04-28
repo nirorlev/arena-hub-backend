cd242b22b00cfca0d898bea2d173dc8507d76d13
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-04-28 23:24:58 +0300

    VCP-15416: add swagger annotation

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/request/VideoPlayDto.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/request/VideoPlayDto.java
@@ -1,14 +1,24 @@
 package com.threeatom.guidecore.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel(description = "Data Transfer Object for Video Play segment")
 public class VideoPlayDto {
+    @ApiModelProperty(value = "Session ID", required = true)
     private UUID sessionId;
+
+    @ApiModelProperty(value = "Segment ID", required = true)
     private Integer segmentId;
+
+    @ApiModelProperty(value = "Start Watch Time in Seconds", required = true)
     private Integer startWatchTimeInSeconds;
+
+    @ApiModelProperty(value = "End Watch Time in Seconds", required = true)
     private Integer endWatchTimeInSeconds;
 }
