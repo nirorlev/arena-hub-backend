8477dd684138f0391fbdd22362c7a582cc391c85
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-05-03 20:11:26 +0300

    VCP-15416: use upsert instead of separate insert and update for segment

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/VideoPlaySessionService.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/VideoPlaySessionService.java
@@ -6,11 +6,9 @@ import com.threeatom.guidecore.entity.GcUser;
 import com.threeatom.guidecore.entity.VideoPlaySession;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface VideoPlaySessionService extends IService<VideoPlaySession> {
     void saveVideoPlaySession(VideoPlayDto videoPlayDto, GcUser user, Integer videoId, Integer masterId);
 
-    @Transactional(readOnly = true)
     Optional<VideoPlaySession> getVideoPlaySession(UUID sessionId);
 }
--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/VideoPlaySegmentServiceImpl.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/VideoPlaySegmentServiceImpl.java
@@ -1,12 +1,10 @@
 package com.threeatom.guidecore.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.threeatom.common.exception.VideoPlaySegmentNotFoundException;
 import com.threeatom.common.exception.VideoPlaySegmentNotUpdatedException;
 import com.threeatom.guidecore.dto.request.VideoPlayDto;
 import com.threeatom.guidecore.entity.GcUser;
 import com.threeatom.guidecore.entity.VideoPlaySegment;
-import com.threeatom.guidecore.entity.VideoPlaySegmentId;
 import com.threeatom.guidecore.entity.VideoPlaySession;
 import com.threeatom.guidecore.mapper.VideoPlaySegmentMapper;
 import com.threeatom.guidecore.mapping.VideoPlaySegmentMapping;
@@ -34,32 +32,23 @@ public class VideoPlaySegmentServiceImpl extends ServiceImpl<VideoPlaySegmentMap
             videoPlaySessionService.getVideoPlaySession(videoPlayDto.getSessionId());
 
         if (videoPlaySessionOptional.isPresent()) {
-            updateVideoPlaySegment(videoPlayDto);
+            updateVideoPlaySegment(videoPlayDto, videoPlaySessionOptional.get());
             return;
         }
 
         videoPlaySessionService.saveVideoPlaySession(videoPlayDto, user, videoId, masterId);
         VideoPlaySession videoPlaySession = videoPlaySessionService.getById(videoPlayDto.getSessionId());
-        this.baseMapper.saveSegment(videoPlaySegmentMapping.map(videoPlayDto, videoPlaySession));
+        this.baseMapper.saveOrUpdateSegment(videoPlaySegmentMapping.map(videoPlayDto, videoPlaySession));
     }
 
-    private void updateVideoPlaySegment(VideoPlayDto videoPlayDto) {
-        VideoPlaySegmentId videoPlaySegmentId = videoPlaySegmentMapping.map(videoPlayDto);
-        Optional<VideoPlaySegment> segment = this.baseMapper.getSegment(videoPlaySegmentId);
-
-        if (segment.isEmpty()) {
-            throw new VideoPlaySegmentNotFoundException(
-                String.format("Video play segment with id '%s' and session '%s' is not found",
-                    videoPlayDto.getSegmentId(),
-                    videoPlayDto.getSessionId()));
-        }
-
-        VideoPlaySegment videoPlaySegment = segment.get();
-        videoPlaySegmentMapping.mapUpdateSegment(videoPlaySegment, videoPlayDto);
-        if (this.baseMapper.updateSegment(videoPlaySegment) == 0) {
-            log.error("Video play segment with id '{}' and session '{}' was not updated",
+    private void updateVideoPlaySegment(VideoPlayDto videoPlayDto, VideoPlaySession videoPlaySession) {
+        if (this.baseMapper.saveOrUpdateSegment(videoPlaySegmentMapping.map(videoPlayDto, videoPlaySession)) == 0) {
+            log.error(
+                "Video play segment id '{}', session '{}' and play segments start '{}' and  end - '{}' was not updated",
                 videoPlayDto.getSegmentId(),
-                videoPlayDto.getSessionId());
+                videoPlayDto.getSessionId(),
+                videoPlayDto.getStartWatchTimeInSeconds(),
+                videoPlayDto.getEndWatchTimeInSeconds());
 
             throw new VideoPlaySegmentNotUpdatedException(
                 String.format("Video play segment with id '%s' and session '%s' was not updated",
