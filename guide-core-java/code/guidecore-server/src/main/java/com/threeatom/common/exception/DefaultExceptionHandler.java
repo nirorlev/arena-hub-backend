80ad916c57a8f61e3add0fe65fc961aab77450cb
Carlos <carlos.costa@powtoon.com>
2024-10-15 09:59:43 -0300

    Improving errors and logs reporting

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/common/exception/DefaultExceptionHandler.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/common/exception/DefaultExceptionHandler.java
@@ -35,6 +35,10 @@ public class DefaultExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
     public Message handlerException(Exception e) {
+        String errorMessage = e.getMessage();
+        if (errorMessage != null) {
+            LOGGER.error(errorMessage, e);
+        }
         sendExceptionToSentry(e);
         return (new Message()).commonError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Sorry, something is wrong!", e);
     }
