825e5ae850b55835d7935002227d6e6e94375a02
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-10-28 19:58:28 +0200

    VCP-17830: update count channel,playlist,video queries for analytics

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/request/AnalyticsFilterDto.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/request/AnalyticsFilterDto.java
@@ -4,7 +4,6 @@ import com.threeatom.common.validation.annotation.AggregateByWithStep;
 import com.threeatom.guidecore.enums.AnalyticsAggregation;
 import java.util.ArrayList;
 import java.util.List;
-import javax.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
