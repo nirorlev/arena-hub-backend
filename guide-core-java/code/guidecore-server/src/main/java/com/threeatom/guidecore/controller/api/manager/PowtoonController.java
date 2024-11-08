5346a74bdfdbc677502f567c4fcbb2d355f0b461
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-11-08 20:16:05 +0200

    fix import

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/controller/api/manager/PowtoonController.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/controller/api/manager/PowtoonController.java
@@ -139,7 +139,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
