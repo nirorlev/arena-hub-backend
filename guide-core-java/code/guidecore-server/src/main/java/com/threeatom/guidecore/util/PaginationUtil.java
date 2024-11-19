6dcb5f884c72bc412524815b38a091ac0a03329f
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-11-19 12:30:47 +0200

    VCP-16767: update hasNextPage caclculation

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/util/PaginationUtil.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/util/PaginationUtil.java
@@ -11,18 +11,25 @@ public class PaginationUtil {
     public <T, U> PageableDto<T> createPageableDto(List<U> items, int totalCount, int pageSize,
                                                    Function<List<U>, List<T>> itemsConverter,
                                                    Function<List<U>, String> cursorExtractor) {
+        List<T> convertedItems = itemsConverter.apply(items);
+        boolean hasNextPage = hasNextPage(convertedItems, pageSize);
+
+        if (hasNextPage) {
+            convertedItems = convertedItems.subList(0, pageSize);
+        }
+
         return PageableDto.<T>builder()
-            .results(itemsConverter.apply(items))
+            .results(convertedItems)
             .pagination(PaginationDto.builder()
                 .count(totalCount)
                 .pageSize(pageSize)
                 .cursor(cursorExtractor.apply(items))
-                .hasNextPage(hasNextPage(items, pageSize, totalCount))
+                .hasNextPage(hasNextPage)
                 .build())
             .build();
     }
 
-    private <T> boolean hasNextPage(List<T> items, int pageSize, long totalCount) {
-        return false;
+    private <T> boolean hasNextPage(List<T> items, int pageSize) {
+        return items.size() > pageSize;
     }
 }
