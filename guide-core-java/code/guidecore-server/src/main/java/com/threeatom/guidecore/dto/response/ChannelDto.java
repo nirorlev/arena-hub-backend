60dc9c5aa92871e1077abdf7ad6190985a5bed70
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-11-25 19:38:04 +0200

    VCP-18220: add base channel dto

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/response/ChannelDto.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/dto/response/ChannelDto.java
@@ -10,12 +10,27 @@ import lombok.Setter;
 @ApiModel(description = "Data Transfer Object representing a Channel")
 @Getter
 @Setter
-public class ChannelWithDetailsDto extends ChannelDto {
+public class ChannelDto {
+    @ApiModelProperty(value = "Unique identifier of the Channel")
+    private Integer id;
+
+    @ApiModelProperty(value = "Title of the Channel")
+    private String title;
+
+    @ApiModelProperty(value = "Slug of the Channel")
+    private String slug;
+
     @ApiModelProperty(value = "Description of the Channel")
     private String desc;
 
+    @ApiModelProperty(value = "Owner of the Channel")
+    private UserDetailsDto owner;
+
     @ApiModelProperty(value = "Privacy visibility for sharable button")
     private Boolean isPrivate;
+
+    @ApiModelProperty(value = "URL of the Channel's avatar")
+    private String avatarUrl;
 
     @ApiModelProperty(value = "URL of the Channel's background")
     private String backgroundUrl;
@@ -28,4 +43,10 @@ public class ChannelWithDetailsDto extends ChannelDto {
 
     @ApiModelProperty(value = "Time when user subscribed to the Channel")
     private OffsetDateTime subscriptionTime;
+
+    @ApiModelProperty(value = "Creation time of the Channel")
+    private OffsetDateTime createTime;
+
+    @ApiModelProperty(value = "Last update time of the Channel")
+    private OffsetDateTime updateTime;
 }
