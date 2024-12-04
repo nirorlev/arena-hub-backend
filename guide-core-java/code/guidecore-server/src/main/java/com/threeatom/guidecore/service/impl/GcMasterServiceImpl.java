052dc805bebc877b746b98fc92a2c65fa0b9e349
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-12-04 12:58:08 +0200

    VCP-18375: add permission check for playlist page

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/GcMasterServiceImpl.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/service/impl/GcMasterServiceImpl.java
@@ -6,8 +6,10 @@ import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.pagehelper.PageInfo;
 import com.threeatom.common.controller.Message;
+import com.threeatom.common.exception.ForbiddenException;
 import com.threeatom.common.permissions.service.AuthorizationService;
 import com.threeatom.guidecore.constant.EnvType;
+import com.threeatom.guidecore.constant.PermitAction;
 import com.threeatom.guidecore.constant.TableConstant;
 import com.threeatom.guidecore.entity.*;
 import com.threeatom.guidecore.mapper.GcMasterMapper;
@@ -131,128 +133,127 @@ public class GcMasterServiceImpl extends ServiceImpl<GcMasterMapper, GcMaster>
         List<GcSubject> twoList = new ArrayList<>();
         int masterId = request.getIntHeader("masterId");
 
-        try {
-            Integer userId = null;
-            if (Objects.nonNull(user)) {
-                userId = user.getId();
-                params.put("userId", userId);
-            }
-
-            PortalUser portalUser = portalUserService.getByUserAndMasterId(userId, masterId);
-            playlist = gcUserSaveFolderService.getById(playlist.getId());
-            playlist.setPermissions(authorizationService.listPermissions(playlist, portalUser));
-
-            GcUserSaveContent content = new GcUserSaveContent();
-            if (Objects.nonNull(user)) {
-                content =
-                        new GcUserSaveContent(
-                                user.getId(), playlist.getMasterId(), playlist.getId());
-            } else {
-                content =
-                        new GcUserSaveContent(null, playlist.getMasterId(), playlist.getId());
-            }
-            m.addData("gcUserSaveFolder", playlist);
-            m.addData("id", playlist.getId());
-            m.addData("name", playlist.getName());
-            if (envFlag.equals(EnvType.PT.getCode())) {
-                GcUserSaveFolder saveFolder = gcUserSaveFolderService.getById(playlist.getId());
-                GcUser folderUser = gcUserService.getById(saveFolder.getUserId());
-                GcUserInfo gcUserInfo = gcUserInfoService.getById(folderUser.getInfoId());
-                gcUserInfo.setUserId(folderUser.getId());
-                gcUserInfo.setUsername(folderUser.getUsername());
-                if (null != gcUserInfo.getAvatarFileId()) {
-                    gcUserInfo.setAvatarFile(sysFileService.getById(gcUserInfo.getAvatarFileId()));
-                    gcUserInfo
-                            .getAvatarFile()
-                            .setFullFileUrl(sysFileService.getResFullUrl(gcUserInfo.getAvatarFile(), request));
-                }
-                m.addData("userInfo", gcUserInfo);
-                m.addData("lastUpdate", saveFolder.getUpdateTime());
-            }
-            // 获取一级课程
-            List<Integer> oneSubIdList = this.gcUserSaveContentService.getOneSubIdList(content);
-            if (!oneSubIdList.isEmpty()) {
-                params.put("subjectIds", oneSubIdList);
-                List<GcSubject> oneList = subjectService.selectBuildSubject(params, request);
-                newUiGcSubjectService.buildSubject2(
-                        oneList, user.getId(), new SysSystem(), request, EnvType.GC.getCode());
-                PageInfo<GcSubject> subjectPageInfo = new PageInfo<>(oneList);
-                m.addData("oneLevelList", subjectPageInfo);
-            }
-
-            // 获取二级课程
-            List<Integer> twoSubIdList = this.gcUserSaveContentService.getTwoSubIdList(content);
-            if (!twoSubIdList.isEmpty()) {
-                twoList = subjectService.selectTwoSubjectByIds(twoSubIdList, request);
-            }
-            PageInfo<GcSubject> gcSubjectPageInfo = new PageInfo<>(twoList);
-            m.addData("twoLevelList", gcSubjectPageInfo);
-            // 获取视频
-            List<GcUserSaveContent> list =
-                    gcUserSaveContentService.selectContetnByFolderId(content.getFolderId());
-            List<Integer> contentIds = this.gcUserSaveContentService.getVideoIdList(content);
-            List<GcVideo> videos = gcVideoService.findByVideoIds(contentIds);
-            Map<Integer, GcVideo> fileIdToVideo = videos.stream().collect(Collectors.toMap(GcVideo::getFileId, Function.identity()));
-            List<SysFile> videoFiles = videos.stream().map(GcVideo::getVideoFile).collect(Collectors.toList());
-
-            for (GcUserSaveContent userSaveContent : list) {
-                for (SysFile file : videoFiles) {
-                    if (userSaveContent.getFileId().equals(file.getId())) {
-                        GcVideo video = fileIdToVideo.get(file.getId());
-
-                        file.setContentId(userSaveContent.getId());
-                        file.setVideoId(userSaveContent.getContentId());
-                        file.setIsLiked(gcUserVideoActionService.isLikedByUser(userSaveContent.getContentId(), userId) ? 1 : 0);
-                        file.setLikeNum(gcUserVideoActionService.countLikeForVideo(userSaveContent.getContentId()));
-                        file.setCommentNumber(video.getCommentNum());
-                        gcVideoService.updateVideoFilePrivacy(file, video);
-                    }
-                }
-            }
-            m.addData("firstVideoId", contentIds.stream().findFirst());
-
-            int followFlag = TableConstant.COMMON_ZERO;
-            if (CollectionUtils.isNotEmpty(list) && envFlag.equals(EnvType.PT.getCode())) {
-                m.addData("videoNum", list.size());
-            }
-
-            if (Objects.nonNull(user)) {
-                List<Integer> follows =
-                        gcUserSaveContentFollowService.selectFollowPlayList(
-                                user.getId(), masterId);
-                if (CollectionUtils.isNotEmpty(follows)) {
-                    if (follows.contains(playlist.getId())) {
-                        followFlag = TableConstant.COMMON_ONE;
-                    } else {
-                        followFlag = TableConstant.COMMON_ZERO;
-                    }
-                }
-                m.addData("followFlag", followFlag);
-            }
-            if (CollectionUtils.isNotEmpty(list)) {
-                List<GcUserVideoAction> gcVideos = gcUserVideoActionService.countLikeForFiles(contentIds);
-                for (SysFile file : videoFiles) {
-                    for (GcUserVideoAction gcUserVideoAction : gcVideos) {
-                        if (file.getId().equals(gcUserVideoAction.getContentId())) {
-                            file.setLikeNum(gcUserVideoAction.getVideoLikeNum());
-                        }
-                    }
-                    file.setSnapshotUrl(sysFileService.getVideoSnapshotUrl(file));
-                    file.setFullFileUrl(sysFileService.getResFullUrl(file, request));
-                }
-            }
-
-            unavailableVideoService.nullifyVideoData(portalUser, videos);
-            videos.forEach(video -> {
-                Map<String, Boolean> permissions = authorizationService.listPermissions(video, portalUser);
-                video.setPermissions(permissions);
-                video.getVideoFile().setPermissions(permissions);
-            });
-            m.addData("videoList", new PageInfo<>(videoFiles));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new Message().error(e.getMessage());
+        Integer userId = null;
+        if (Objects.nonNull(user)) {
+            userId = user.getId();
+            params.put("userId", userId);
         }
+
+        PortalUser portalUser = portalUserService.getByUserAndMasterId(userId, masterId);
+        playlist = gcUserSaveFolderService.getById(playlist.getId());
+        if (!authorizationService.checkAccess(playlist, PermitAction.VIEW, portalUser)) {
+            throw new ForbiddenException("Playlist is not accessible");
+        }
+
+        playlist.setPermissions(authorizationService.listPermissions(playlist, portalUser));
+
+        GcUserSaveContent content = new GcUserSaveContent();
+        if (Objects.nonNull(user)) {
+            content =
+                    new GcUserSaveContent(
+                            user.getId(), playlist.getMasterId(), playlist.getId());
+        } else {
+            content =
+                    new GcUserSaveContent(null, playlist.getMasterId(), playlist.getId());
+        }
+        m.addData("gcUserSaveFolder", playlist);
+        m.addData("id", playlist.getId());
+        m.addData("name", playlist.getName());
+        if (envFlag.equals(EnvType.PT.getCode())) {
+            GcUserSaveFolder saveFolder = gcUserSaveFolderService.getById(playlist.getId());
+            GcUser folderUser = gcUserService.getById(saveFolder.getUserId());
+            GcUserInfo gcUserInfo = gcUserInfoService.getById(folderUser.getInfoId());
+            gcUserInfo.setUserId(folderUser.getId());
+            gcUserInfo.setUsername(folderUser.getUsername());
+            if (null != gcUserInfo.getAvatarFileId()) {
+                gcUserInfo.setAvatarFile(sysFileService.getById(gcUserInfo.getAvatarFileId()));
+                gcUserInfo
+                        .getAvatarFile()
+                        .setFullFileUrl(sysFileService.getResFullUrl(gcUserInfo.getAvatarFile(), request));
+            }
+            m.addData("userInfo", gcUserInfo);
+            m.addData("lastUpdate", saveFolder.getUpdateTime());
+        }
+        // 获取一级课程
+        List<Integer> oneSubIdList = this.gcUserSaveContentService.getOneSubIdList(content);
+        if (!oneSubIdList.isEmpty()) {
+            params.put("subjectIds", oneSubIdList);
+            List<GcSubject> oneList = subjectService.selectBuildSubject(params, request);
+            newUiGcSubjectService.buildSubject2(
+                    oneList, user.getId(), new SysSystem(), request, EnvType.GC.getCode());
+            PageInfo<GcSubject> subjectPageInfo = new PageInfo<>(oneList);
+            m.addData("oneLevelList", subjectPageInfo);
+        }
+
+        // 获取二级课程
+        List<Integer> twoSubIdList = this.gcUserSaveContentService.getTwoSubIdList(content);
+        if (!twoSubIdList.isEmpty()) {
+            twoList = subjectService.selectTwoSubjectByIds(twoSubIdList, request);
+        }
+        PageInfo<GcSubject> gcSubjectPageInfo = new PageInfo<>(twoList);
+        m.addData("twoLevelList", gcSubjectPageInfo);
+        // 获取视频
+        List<GcUserSaveContent> list =
+                gcUserSaveContentService.selectContetnByFolderId(content.getFolderId());
+        List<Integer> contentIds = this.gcUserSaveContentService.getVideoIdList(content);
+        List<GcVideo> videos = gcVideoService.findByVideoIds(contentIds);
+        Map<Integer, GcVideo> fileIdToVideo = videos.stream().collect(Collectors.toMap(GcVideo::getFileId, Function.identity()));
+        List<SysFile> videoFiles = videos.stream().map(GcVideo::getVideoFile).collect(Collectors.toList());
+
+        for (GcUserSaveContent userSaveContent : list) {
+            for (SysFile file : videoFiles) {
+                if (userSaveContent.getFileId().equals(file.getId())) {
+                    GcVideo video = fileIdToVideo.get(file.getId());
+
+                    file.setContentId(userSaveContent.getId());
+                    file.setVideoId(userSaveContent.getContentId());
+                    file.setIsLiked(gcUserVideoActionService.isLikedByUser(userSaveContent.getContentId(), userId) ? 1 : 0);
+                    file.setLikeNum(gcUserVideoActionService.countLikeForVideo(userSaveContent.getContentId()));
+                    file.setCommentNumber(video.getCommentNum());
+                    gcVideoService.updateVideoFilePrivacy(file, video);
+                }
+            }
+        }
+        m.addData("firstVideoId", contentIds.stream().findFirst());
+
+        int followFlag = TableConstant.COMMON_ZERO;
+        if (CollectionUtils.isNotEmpty(list) && envFlag.equals(EnvType.PT.getCode())) {
+            m.addData("videoNum", list.size());
+        }
+
+        if (Objects.nonNull(user)) {
+            List<Integer> follows =
+                    gcUserSaveContentFollowService.selectFollowPlayList(
+                            user.getId(), masterId);
+            if (CollectionUtils.isNotEmpty(follows)) {
+                if (follows.contains(playlist.getId())) {
+                    followFlag = TableConstant.COMMON_ONE;
+                } else {
+                    followFlag = TableConstant.COMMON_ZERO;
+                }
+            }
+            m.addData("followFlag", followFlag);
+        }
+        if (CollectionUtils.isNotEmpty(list)) {
+            List<GcUserVideoAction> gcVideos = gcUserVideoActionService.countLikeForFiles(contentIds);
+            for (SysFile file : videoFiles) {
+                for (GcUserVideoAction gcUserVideoAction : gcVideos) {
+                    if (file.getId().equals(gcUserVideoAction.getContentId())) {
+                        file.setLikeNum(gcUserVideoAction.getVideoLikeNum());
+                    }
+                }
+                file.setSnapshotUrl(sysFileService.getVideoSnapshotUrl(file));
+                file.setFullFileUrl(sysFileService.getResFullUrl(file, request));
+            }
+        }
+
+        unavailableVideoService.nullifyVideoData(portalUser, videos);
+        videos.forEach(video -> {
+            Map<String, Boolean> permissions = authorizationService.listPermissions(video, portalUser);
+            video.setPermissions(permissions);
+            video.getVideoFile().setPermissions(permissions);
+        });
+        m.addData("videoList", new PageInfo<>(videoFiles));
         return m.ok("查询成功");
     }
 }
