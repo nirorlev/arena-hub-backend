c2b398b5a1facee0eab0d5bfdcf4f16b09ddcaa6
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2024-12-19 18:32:04 +0200

    VCP-18200: update priority for frontend version fetching

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/util/RequestUtil.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/util/RequestUtil.java
@@ -6,7 +6,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.experimental.UtilityClass;
-import org.springframework.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -15,7 +15,7 @@ public class RequestUtil {
 
     private static final String MASTER_ID = "masterId";
     private static final String AUTHORIZATION = "Authorization";
-    private static final String FE_VERSION_OVERRIDE_NAME = "frontendVersion";
+    private static final String REQUESTED_FRONTEND_VERSION_NAME = "frontendVersion";
 
     public static String getRequestAuthHeader(HttpServletRequest request) {
         return request.getHeader(AUTHORIZATION);
@@ -49,11 +49,12 @@ public class RequestUtil {
     }
 
     public static String getRequestedFrontendVersion(HttpServletRequest request, HttpServletResponse response) {
-        return getCookieValue(request, FE_VERSION_OVERRIDE_NAME)
-            .orElseGet(() -> {
-                String feVersion = request.getParameter(FE_VERSION_OVERRIDE_NAME);
-                response.addCookie(new Cookie(FE_VERSION_OVERRIDE_NAME, feVersion));
-                return feVersion;
-            });
+        String requestedVersion = request.getParameter(REQUESTED_FRONTEND_VERSION_NAME);
+        if (StringUtils.isNotBlank(requestedVersion)) {
+            response.addCookie(new Cookie(REQUESTED_FRONTEND_VERSION_NAME, requestedVersion));
+            return requestedVersion;
+        }
+
+        return getCookieValue(request, REQUESTED_FRONTEND_VERSION_NAME).orElse(null);
     }
 }
