314a0be6792d5d67234b97ef7cf1abcb25f33d3a
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2025-01-09 12:11:14 +0200

    VCP-18528: update endpoint path and payload

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/controller/api/CourseController.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/controller/api/CourseController.java
@@ -1,6 +1,5 @@
 package com.threeatom.guidecore.controller.api;
 
-import com.threeatom.guidecore.dto.request.IdDto;
 import com.threeatom.guidecore.entity.GcUser;
 import com.threeatom.guidecore.entity.PortalUser;
 import com.threeatom.guidecore.service.CourseEnrollmentService;
@@ -9,12 +8,11 @@ import com.threeatom.guidecore.service.PortalUserService;
 import com.threeatom.guidecore.util.RequestUtil;
 import io.swagger.annotations.Api;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,13 +26,13 @@ public class CourseController {
     private final GcUserService userService;
     private final PortalUserService portalUserService;
 
-    @PostMapping("/enroll")
-    public ResponseEntity<Void> enrollToCourse(@RequestBody @Valid IdDto dto, HttpServletRequest request) {
+    @PostMapping("/{courseId}/users/")
+    public ResponseEntity<Void> enrollToCourse(@PathVariable Integer courseId, HttpServletRequest request) {
         Integer masterId = RequestUtil.getMasterId(request).orElseThrow();
         GcUser currentUser = userService.getCurrentUser(request);
         PortalUser portalUser = portalUserService.getByUserAndMasterId(currentUser.getId(), masterId);
 
-        courseEnrollmentService.enrollToCourse(portalUser, dto.getId());
+        courseEnrollmentService.enrollToCourse(portalUser, courseId);
 
         return ResponseEntity.ok().build();
     }
