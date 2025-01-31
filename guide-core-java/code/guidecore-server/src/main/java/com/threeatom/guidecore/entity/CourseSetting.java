edae600947b794526039a19226f10595b70637e4
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2025-01-31 15:22:18 +0200

    VCP-18885: fix course setting migration

--- guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/entity/CourseSetting.java
+++ guide-core-java/code/guidecore-server/src/main/java/com/threeatom/guidecore/entity/CourseSetting.java
@@ -1,15 +1,22 @@
 package com.threeatom.guidecore.entity;
 
-
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @TableName(value = "course_settings", autoResultMap = true)
-public class CourseSetting {
+public class CourseSetting implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
+
     private Integer courseId;
 
     private Integer sectionContentStudiedPercentage;
