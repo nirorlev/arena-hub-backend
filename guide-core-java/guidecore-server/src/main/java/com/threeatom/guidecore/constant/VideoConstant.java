12cdbe0fd595e2af430567050fd37a3fe58e4bba
Vlad Prokhnitskyi <vlad.prokhnitskyi@powtoon.com>
2025-04-16 17:01:53 +0300

    VCP-19605: fix video source

--- guide-core-java/guidecore-server/src/main/java/com/threeatom/guidecore/constant/VideoConstant.java
+++ guide-core-java/guidecore-server/src/main/java/com/threeatom/guidecore/constant/VideoConstant.java
@@ -6,5 +6,5 @@ public class VideoConstant {
 
     public static final int TECENT = 2;
     public static final int SCREENROCK = 3;
-    public static final String GCVIDEO_VIDEOSOURCE_jsonStr = "[1,2,3,5,6,7,8,9,10]";
+    public static final String GCVIDEO_VIDEOSOURCE_jsonStr = "[1,2,3,5,6,7,8,9,10,13]";
 }
